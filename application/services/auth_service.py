c73f9afb518adc56ae05539218fe2e735c02951d
Jheyson Perez <jheyson.xcalibur.15@gmail.com>
2026-03-24 06:17:38 -0500

    Actualización

--- application/services/auth_service.py
+++ application/services/auth_service.py
@@ -1,5 +1,8 @@
-ADMIN_EMAIL = "jheyson.xcalibur.15@gmail.com"
+ADMIN_EMAILS = [
+    "jheyson.xcalibur.15@gmail.com",
+    "infoysys@gmail.com"
+]
 
 # Verifica si el email es del administrador
 def is_authorized_user(email):
-    return email == ADMIN_EMAIL
+    return email.lower() in [admin.lower() for admin in ADMIN_EMAILS]
--- presentation/routes/private/admin_guard.py
+++ presentation/routes/private/admin_guard.py
@@ -1,7 +1,10 @@
 from functools import wraps
 from flask import session, redirect, url_for
 
-ADMIN_EMAIL = "jheyson.xcalibur.15@gmail.com"
+ADMIN_EMAILS = [
+    "jheyson.xcalibur.15@gmail.com",
+    "infoysys@gmail.com"
+]
 
 
 def admin_required(f):
@@ -13,7 +16,9 @@ def admin_required(f):
         if not user:
             return redirect(url_for("auth.login_google"))
 
-        if user.get("email") != ADMIN_EMAIL:
+        email = user.get("email", "").lower()
+
+        if email not in [admin.lower() for admin in ADMIN_EMAILS]:
             return redirect(url_for("auth.logout"))
 
         return f(*args, **kwargs)
