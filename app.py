c2bc5bc25966f70fcc65ae8f8b3cb6d70717c40f
Jheyson Perez <jheyson.xcalibur.15@gmail.com>
2026-03-24 01:48:41 -0500

    app correguido

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ from config import Config
 from infrastructure.database.postgres import db
 from infrastructure.mail.smtp import mail
 
+from domain.models.statistic import Statistic
+from domain.models.course import Course
+from domain.models.news import News
+from domain.models.services import Service
+from domain.models.team_member import TeamMember
+from domain.models.contact_message import ContactMessage
+
 from presentation.routes.public.home_routes import home_bp
 from presentation.routes.private.admin_team_routes import admin_team_bp
 from presentation.routes.private.admin_news_routes import admin_news_bp
@@ -20,7 +27,6 @@ from presentation.routes.public.auth_routes import auth_bp, oauth
 from presentation.routes.private.admin_routes import admin_bp
 from presentation.routes.private.admin_services_routes import admin_services_bp
 
-
 def create_app():
     base_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -31,14 +37,11 @@ def create_app():
     )
 
     app.config.from_object(Config)
-
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "super_secret_key")
-
     app.config["SESSION_COOKIE_NAME"] = "infosys_session"
     app.config["SESSION_COOKIE_HTTPONLY"] = True
     app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
     app.config["SESSION_COOKIE_SECURE"] = False
-
     app.config["JSON_AS_ASCII"] = False
 
     @app.after_request
@@ -50,6 +53,9 @@ def create_app():
     db.init_app(app)
     mail.init_app(app)
     oauth.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     @app.context_processor
     def inject_user():
@@ -72,8 +78,7 @@ def create_app():
 
     return app
 
-
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
